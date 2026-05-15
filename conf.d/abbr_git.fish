a02d21b95bc0cb9884289d71a4931f3eaa45b8e3
Michael Morgan <self@msmorgan.dev>
2026-05-14 19:12:05 -0700

    Add a few git abbreviations

---
+++ conf.d/abbr_git.fish
@@ -0,0 +1,10 @@
+abbr -a gc 'git commit'
+abbr -a gca 'git commit --all'
+abbr -a gc! 'git commit --amend'
+abbr -a gca! 'git commit --all --amend'
+abbr -a gcan! 'git commit --all --amend --no-edit'
+
+abbr -a grbc 'git rebase --continue'
+abbr -a grba 'git rebase --abort'
+abbr -a grb-auto 'git rebase --interactive --autosquash --autostash --root'
+
