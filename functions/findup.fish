7024fec24c7121fa5f4d50c8475f7e779dc3bc53
Michael Morgan <self@msmorgan.dev>
2026-05-19 20:25:18 -0700

    `findup`: add `-p/--parent` to return the containing directory

--- functions/findup.fish
+++ functions/findup.fish
@@ -1,12 +1,19 @@
 function findup --description 'Traverse parent directories to find a path.'
-    argparse --strict-longopts --min-args 1 --max-args 1 \
-        "r/relative" \
-        -- $argv
+    argparse -S -N1 -X1 r/relative p/parent -- $argv
     or return
 
     set needle (path normalize $argv[1])
     if string match -qr '^/' $needle
-        path filter $needle
+        if set -q _flag_relative
+            echo >&2 (status function): "absolute path incompatible with --relative"
+            return 1
+        end
+
+        if set -q _flag_parent
+            path filter (path normalize $needle/..)
+        else
+            path filter $needle
+        end
         return
     end
 
@@ -23,9 +30,16 @@ function findup --description 'Traverse parent directories to find a path.'
         end
     end
 
-    if set -qf _flag_relative
-        echo (path normalize $haystack/$needle)
+    if set -q _flag_parent
+        set result $haystack
     else
-        echo (path resolve $haystack/$needle)
+        set result $haystack/$needle
     end
+
+    if set -q _flag_relative
+        path normalize $result
+    else
+        path resolve $result
+    end
+    return 0
 end
