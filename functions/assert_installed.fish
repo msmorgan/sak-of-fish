7d2242c851c8e1b3e697fbf0e2ddb63b4cd1d5d6
Michael Morgan <self@msmorgan.dev>
2026-05-19 20:25:07 -0700

    `assert_installed`: check required commands present

---
+++ functions/assert_installed.fish
@@ -0,0 +1,17 @@
+function assert_installed
+    argparse -S f/function= -- $argv
+    or return
+
+    set prefix $_flag_function:' '
+
+    for cmd in $argv
+        command -q $cmd
+        or set -a errors "$prefix$cmd not installed"
+    end
+
+    if set -q errors[1]
+        string collect >&2 -- $errors
+        return 1
+    end
+end
+
